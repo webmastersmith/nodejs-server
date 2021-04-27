2677eb422367973248207bd51e5452fae323e5f0
webmastersmith <bryon@smithauto.us>
2021-04-26 20:56:47 -0500

    add basic crud operations

--- src/index.js
+++ src/index.js
@@ -8,7 +8,25 @@ app.use(cors())
  
 app.get('/', (req, res) => {
     res.send('Hello World!!!!');
-  });
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+  
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+app.put('/users/:userId', (req, res) => {
+  return res.send(`Received a PUT Wildcard HTTP method ${req.params.userId}\n`);
+});
+  
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+app.delete('/users/:userId', (req, res) => {
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource\n`);
+});
 
 app.listen(process.env.PORT, () =>
 console.log(`Example app listening on port ${process.env.PORT}!`),
