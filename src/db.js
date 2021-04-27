1af84a2fa8335e42079a9853c4770f30da205882
webmastersmith <bryon@smithauto.us>
2021-04-27 10:53:47 -0500

    adding post commands

---
+++ src/db.js
@@ -0,0 +1,23 @@
+export const users = {
+    1: {
+      id: '1',
+      username: 'Bob Jones',
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+    },
+  };
+   
+ export const messages = {
+    1: {
+      id: '1',
+      text: 'Hello World',
+      userId: '1',
+    },
+    2: {
+      id: '2',
+      text: 'By World',
+      userId: '2',
+    },
+  };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,60 @@
 import 'dotenv/config';
 import cors from 'cors'
+import {v4} from 'uuid'
 import express from 'express';
+import {users, messages} from './db'
  
 const app = express();
 
 app.use(cors())
  
-app.get('/', (req, res) => {
-    res.send('Hello World!!!!');
+app.use(express.static('public'))
+
+// next middleware
+app.use((req,res,next) => {
+  req.me = users[1]
+  next()
+})
+
+// req.body.text is turned into JSON data for you, otherwise you get the whole html page.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// get request
+app.get('/users', (req, res) => {
+    res.send(JSON.stringify(Object.values(users)) + '\n');
+});
+app.get('/users/:userId', (req, res) => {
+    res.send(JSON.stringify(users[req.params.userId]) + '\n');
+});
+app.get('/messages', (req, res) => {
+    res.send(JSON.stringify(Object.values(messages)) + '\n');
+});
+app.get('/messages/:messageId', (req, res) => {
+    res.send(JSON.stringify(messages[req.params.messageId]) + '\n');
 });
 
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+
+
+// post request
+app.post('/messages', (req, res) => {
+  const id = v4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id,
+    userId: req.me.username,
+  }
+  messages[id] = message
+  return res.send(message);
 });
-  
+
+
+
+
+
+
 app.put('/', (req, res) => {
   return res.send('Received a PUT HTTP method');
 });
@@ -34,5 +75,6 @@ console.log(`Example app listening on port ${process.env.PORT}!`),
 
 
 console.log('Hello Node.js project.');
+// console.log(v4())
  
 console.log(process.env.MY_SECRET);
