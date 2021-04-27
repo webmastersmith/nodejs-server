11ab87c863652992d83c3870932fdb0806c872f0
webmastersmith <bryon@smithauto.us>
2021-04-26 19:29:39 -0500

    added cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import 'dotenv/config';
+import cors from 'cors'
 import express from 'express';
  
 const app = express();
+
+app.use(cors())
  
+app.get('/', (req, res) => {
+    res.send('Hello World!!!!');
+  });
+
 app.listen(process.env.PORT, () =>
 console.log(`Example app listening on port ${process.env.PORT}!`),
 );
+
 
 console.log('Hello Node.js project.');
  
